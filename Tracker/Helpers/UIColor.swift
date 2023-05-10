67a3b8dfba0095205bc2e865f8a3ddd3e1b4aaab
Shishmakova Daria <dary_shishka@mail.ru>
2023-05-11 00:07:53 +0300

    Добавлен LaunchScreen

---
+++ Tracker/Helpers/UIColor.swift
@@ -0,0 +1,13 @@
+//
+//  UIColor.swift
+//  Tracker
+//
+//  Created by Дарья Шишмакова on 10.05.2023.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static var ypBlue: UIColor {UIColor(named: "Blue")!}
+}
